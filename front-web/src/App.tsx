2b78a78d297b4e86d44ac5c1bc34f789f21b8647
Felipe Gonzales <felipe.gonzales37@gmail.com>
2021-01-10 01:02:31 -0300

    Index corrections

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import Navbar from './navbar';
 import Routes from './Routes';
-import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
