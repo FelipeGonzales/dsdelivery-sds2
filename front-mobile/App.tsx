c4f925105092c716f5c5df12de71b53052e2973c
Felipe Gonzales <felipe.gonzales37@gmail.com>
2021-01-13 00:06:53 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,13 +1,13 @@
-import { useFonts, OpenSans_400Regular, OpenSans_700Bold  } from '@expo-google-fonts/open-sans';
-import AppLoading from 'expo-app-loading';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/header';
-import Home from './src/Home/Index';
+import AppLoading from 'expo-app-loading';
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 
+import Routes from './src/Routes';
 
 export default function App() {
+
   let [fontsLoaded] = useFonts({
     OpenSans_400Regular,
     OpenSans_700Bold
@@ -16,12 +16,11 @@ export default function App() {
   if (!fontsLoaded) {
     return <AppLoading />;
   }
-  
+
   return (
     <View style={styles.container}>
-     <StatusBar style="auto" />
-     <Header />
-     <Home />
+      <StatusBar style="auto" />
+      <Routes />
     </View>
   );
 }
--- front-mobile/src/header/index.tsx
+++ front-mobile/src/header/index.tsx
@@ -1,13 +1,23 @@
 
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { LogBox, StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
-function Header() {
+export default function Header() {
+  const navigation = useNavigation();
+
+  const handleOnPress = () => {
+      navigation.navigate('Home');
+  }
+  
   return (
-    <View style={styles.container}>
-      <Image source={require('../assets/logo.png')} /> 
-      <Text style={styles.text}>DS Delivery</Text>
-    </View>
+    <TouchableWithoutFeedback>
+      <View style={styles.container}>
+        <Image source={require('../assets/logo.png')}/>
+        <Text style={styles.text}>DS Delivery</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -15,19 +25,17 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#DA5C5C',
     height: 90,
-    paddingTop:50,
+    paddingTop: 50,
     flexDirection: 'row',
     justifyContent: 'center'
   },
   text: {
-      fontWeight: 'bold',
-      fontSize: 18,
-      lineHeight: 25,
-      letterSpacing: -0.24,
-      color: '#FFF',
-      marginLeft: 15,   
-      fontFamily: 'OpenSans_700Bold' 
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 25,
+    letterSpacing: -0.24,
+    color: '#FFF',
+    marginLeft: 15,
+    fontFamily: 'OpenSans_700Bold'
   }
-}); 
-
-export default Header;
+});
